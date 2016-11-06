e61d73243e89a34462a3859c1d8fb25b55704ce7
Jason Schapiro <yono38@gmail.com>
2016-11-06 12:40:10 -0500

    chore(typo): Fix typo

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require('express');
+// process.env.PWD = process.cwd()
 
 module.exports = {
   app: function () {
     const app = express();
-    const indexPath = path.join(__dirname, '/build/index.html');
-    const publicPath = path.join(__dirname, 'build');
-    app.use(express.static(publicPath));
-    app.get('/', function (_, res) { res.sendFile(indexPath) });
+    // const indexPath = path.join(__dirname, '/build/index.html');
+    // const publicPath = path.join(__dirname, 'build');
+    app.use(express.static(process.cwd() + '/build'));
+
+    // app.use(express.static(publicPath));
+    // app.get('/', function (_, res) { res.sendFile(indexPath) });
 
     return app;
   }
