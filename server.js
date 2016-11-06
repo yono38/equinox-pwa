577aeb536fd0a54f80ec7b81cd914f1c79ea12ef
Jason Schapiro <yono38@gmail.com>
2016-11-06 12:36:07 -0500

    chore(index): typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ module.exports = {
   app: function () {
     const app = express();
     const indexPath = path.join(__dirname, '/build/index.html');
-    const publicPath = path.join(__dirname, 'build'));
+    const publicPath = path.join(__dirname, 'build');
     app.use(express.static(publicPath));
     app.get('/', function (_, res) { res.sendFile(indexPath) });
 
