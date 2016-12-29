2a152b6b5742557ab40769292143b396cdf0635c
Jason Schapiro <yono38@gmail.com>
2016-12-29 17:08:41 -0500

    chore(server): Server fallback to index.html for react router

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const express = require('express');
 module.exports = {
   app: function () {
     const app = express();
-    // const indexPath = path.join(__dirname, '/build/index.html');
     // const publicPath = path.join(__dirname, 'build');
     app.use(express.static(process.cwd() + '/build'));
+
+    // send all requests to index.html so browserHistory in React Router works
+    app.get('*', function (req, res) {
+      const indexPath = path.join(__dirname, '/build/index.html');
+      res.sendFile(indexPath);
+    })
 
     // app.use(express.static(publicPath));
     // app.get('/', function (_, res) { res.sendFile(indexPath) });
