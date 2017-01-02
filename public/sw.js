5ec63ab27d2aa04b0a3233c8063b14ef636b96d8
Jason Schapiro <yono38@gmail.com>
2017-01-02 00:29:39 -0500

    feature(sw): Update service worker to link to booking page on notification click

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,16 @@
 // TODO use this array to persist notifications
 // on reload with localStorage using metadata to recreate
-self.importScripts('service-worker.js');
+try {
+  self.importScripts('service-worker.js');
+} catch(err) {
+  console.log('[Service Worker] Failed to import precache SW')
+}
 
 let notificationPromises = [];
 
 self.addEventListener('message', function(event) {
     var data = event.data;
-    console.log("Received Event: ", data);
+    console.log("[Service Worker] Received Event: ", data);
 
     if (data.command == "addNotification") {
       function delayFunction(time) {
@@ -14,10 +18,10 @@ self.addEventListener('message', function(event) {
           setTimeout(fulfill, time);
         });
       }
-      console.log("Add Notification: ", data.name);
+      console.log("[Service Worker] Add Notification: ", data.name);
       const delayTime = data.delay || 0;
       const bookAlert = delayFunction(delayTime).then(() => {
-        console.log('Show Notification')
+        console.log('[Service Worker] Show Notification')
         const title = data.name || 'Test Notification';
         self.registration.showNotification(title, {
           body: 'Spots are open to book for this class',
@@ -34,4 +38,14 @@ self.addEventListener('message', function(event) {
         }
       });
     }
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+  const bookingUrl = 'https://equinox-pwa.herokuapp.com/classes?bookable=true';
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow(bookingUrl)
+  );
 });
